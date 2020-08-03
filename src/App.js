6fc31b939680a17c8695bde97cdea35e6561c913
ktm07 <ktm7@hw.ac.uk>
2020-08-03 10:46:46 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import{
+  MenuItem,
+  FormControl,
+  Select, 
+} from "@material-ui/core";
 import './App.css';
 
 function App() {
+  const[countries, setCountries]= useState([]);
+  const[country, setCountry]= useState('Worldwide');
+
+    // https://disease.sh/v3/covid-19/countries
+
+    // Useeffect = Runs a piece of code based on a given condition
+
+    useEffect(() => {
+      // The code inside here will run once
+      // when the component loads and not again
+      const getCountriesData = async () => {
+        await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries =data.map((country) => ({
+              name: country.country, //United States, United Kingdom
+              value:country.countryInfo.iso2 //UK, USA, FR
+            }
+          ));
+          setCountries(countries);
+        })
+      }
+
+      getCountriesData();
+    }, []);
+
+    const onCountryChange = async (event) => {
+      const
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+      <div className="app_header">
+      <h1>COVID-19 Tracker</h1>
+    <FormControl className="app_dropdown">
+      <Select variant="outlined" onChange={} value={"country"}>
+      <MenuItem value="worldwide">Worldwide</MenuItem>
+          {countries.map(country =>(
+            <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          {/* <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Option two</MenuItem>
+          <MenuItem value="worldwide">Option 3</MenuItem>
+          <MenuItem value="worldwide">Option 4</MenuItem> */}
+      </Select>
+    </FormControl>
+
+      </div>
+    
+    {/* Header */}
+    {/* Title + Select input dropdown field */}
+
+    {/* InfoBoxs */}
+    {/* InfoBoxs */}
+    {/* InfoBoxs */}
+
+    {/* Table */}
+    {/* Graph */}
+
+    {/* Map */}
+
     </div>
   );
 }
