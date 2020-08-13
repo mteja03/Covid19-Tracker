dc32178e992e47c3b0b21656180b9f506fecc009
mteja03 <mteja0852@gmail.com>
2020-08-13 11:13:54 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,30 +1,43 @@
 import React, {useState, useEffect} from 'react';
-import{
-  MenuItem,
-  FormControl,
-  Select, 
-} from "@material-ui/core";
+import{ MenuItem, FormControl, Select, Card, CardContent} from "@material-ui/core";
 import './App.css';
+import InfoBox from './InfoBox';
+import Map from './Map';
+import Table from "./Table";
+import { sortData } from './util';
+import LineGraph from "./LineGraph";
+import "leaflet/dist/leaflet.css";
+
 
 function App() {
-  const[countries, setCountries]= useState([]);
-  const[country, setCountry]= useState('Worldwide');
+  const [countries, setCountries]= useState([]);
+  const [country, setCountry]= useState('Worldwide');
+  const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
-    // https://disease.sh/v3/covid-19/countries
-    // Useeffect = Runs a piece of code based on a given condition
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+    .then(response => response.json())
+    .then(data => {
+      setCountryInfo(data);
+    })
+  }, [])
+
 
     useEffect(() => {
-      // The code inside here will run once
-      // when the component loads and not again
       const getCountriesData = async () => {
         await fetch("https://disease.sh/v3/covid-19/countries")
         .then((response) => response.json())
         .then((data) => {
-          const countries =data.map((country) => ({
+          const countries =data.map((country) => (
+            {
               name: country.country, //United States, United Kingdom
               value:country.countryInfo.iso2 //UK, USA, FR
             }
           ));
+
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         })
       }
@@ -34,41 +47,63 @@ function App() {
     }, []);
 
     const onCountryChange = async (event) => {
-      const
-    }
+      const countryCode = event.target.value;
+      
+
+    const url = 
+      countryCode === "worldwide" 
+      ? "https://disease.sh/v3/covid-19/all" 
+      : `https://disease.sh/v3/covid-19/countries/${countryCode}`;
+
+    await fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      setCountry(countryCode);
+      
+      setCountryInfo(data);
+    });
+    };
 
   return (
-    <div className="App"> 
+  <div className="app"> 
+    <div className="app_left">
       <div className="app_header">
-      <h1>COVID-19 Tracker krishana mattapalli</h1>
-    <FormControl className="app_dropdown">
-      <Select variant="outlined" onChange={} value={"country"}>
-      <MenuItem value="worldwide">Worldwide</MenuItem>
-          {countries.map(country =>(
-            <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          {/* <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Option two</MenuItem>
-          <MenuItem value="worldwide">Option 3</MenuItem>
-          <MenuItem value="worldwide">Option 4</MenuItem> */}
-      </Select>
-    </FormControl>
+        <h1>COVID-19 Tracker</h1>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" onChange={onCountryChange} value={country}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) =>(
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+          </Select>
+        </FormControl>
+      </div>
 
+      <div className="app_stats">
+        <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases}/>
+
+        <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered}/>
+
+        <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths}/>
+
+        {/* InfoBoxs title="CoronaVirus cases" */}
+        {/* InfoBoxs title="CoronaVirus recoveries" */}
+        {/* InfoBoxs */}
       </div>
     
-    {/* Header */}
-    {/* Title + Select input dropdown field */}
-
-    {/* InfoBoxs */}
-    {/* InfoBoxs */}
-    {/* InfoBoxs */}
-
-    {/* Table */}
-    {/* Graph */}
-
-    {/* Map */}
-
+          
+       <Map/>
     </div>
+    <Card className="app_right">
+      <CardContent>
+        <h3>Live Cases By Country</h3>
+       <Table countries={tableData}/>
+        <h3>WorldWide New Cases</h3>
+       <LineGraph/>
+      </CardContent>
+
+    </Card>
+  </div>
   );
 }
 
---
+++ src/Map.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import  "./Map.css";
+import { Map as LeafletMap, TileLayer } from "react-leaflet";
+
+function Map({center, zoom}) {
+    return (
+        <div className="map">
+            <LeafletMap center={center} zoom={zoom}>
+                <TileLayer
+                url="https://{s}.title.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'/>
+            </LeafletMap>
+        </div>
+    );
+}
+
+export default Map;
